8ca4769e36be9b152d33a9cd02ef0c6cc400972f
Мoiseev Кirill <kilkuruzzza@mail.ru>
2023-02-12 16:20:33 +0300

    1

--- 9/Program.cs
+++ 9/Program.cs
@@ -1,15 +1,21 @@
-﻿Console.Clear();
+﻿//9 Напишите программу, которая выводит случайное число из отрезка
+// [10, 99]
+// и показывает наибольшую цифру числа.
+
+Console.Clear();
+
 int num = new Random().Next(10, 100);
-Console.WriteLine($"Число {num}");
-int num1 = num / 10;
-int num2 = num % 10;
-if (num1 > num2)
-{
+int a1 = num / 10;
+int a2=num % 10;
+int max = a1;
+if (max < a2)
+    max = a2;
+Console.WriteLine($"Максимальная цифра числа {num} -> {max}");
+//11 Напишите программу, которая выводит случайное 
+// трёхзначное число и удаляет вторую цифру этого числа.
 
+// 456 -> 46
 
-    Console.WriteLine($"Цифра {num1} больше цифры {num2}");
-}
-else
-{
-    Console.WriteLine($"Цифра {num2} больше цифры {num1}");
-}
+// 782 -> 72
+
+// 918 -> 98
--- dop_task/Program.cs
+++ dop_task/Program.cs
@@ -1,6 +1,16 @@
 ﻿// На столе лежат n монеток. Некоторые из них лежат вверх решкой, 
 // а некоторые – гербом. Определите минимальное число монеток, которые нужно перевернуть, 
 // чтобы все монетки были повернуты вверх одной и той же стороной.
+/*
+int r = new Random().Next(1, 10);
+int[] a = new int[r];
+for (int i = 0; i < r;i++){
+    a[i] = i;
+}
+for (int i = 0; i < r; i++){
+    Console.Write($"{i} ");
+}
+*/
 Console.Clear();
 Console.Write("Введите кол-во элементов: ");
 int n = int.Parse(Console.ReadLine());
@@ -8,37 +18,58 @@ int[] array1 = new int[n];
 Console.Write("Массив: ");
 
 
-for (int i = 0; i < n; i++){
+int r = 0;
+for (int i = 0; i < n; i++)
+{
     array1[i] = new Random().Next(0, 2);
-}
-Console.WriteLine("for");
-for (int i = 0; i < n; i++)
-{
     Console.Write($"{array1[i]} ");
-}
-Console.WriteLine();
-Console.WriteLine("while");
-int j = 0;
-while (j < n) {
-    Console.Write($"{array1[j]} ");
-    j++;
-}
-
-
-int r = 0, g = 0;
-for (int i = 0; i < n; i++)
-{
     if (array1[i] == 1){
         r++;
-    }else{
-        g++;
     }
 }
-
 Console.WriteLine();
-if (r < g){
+if (r < n - r){
     Console.Write($"{r}");
+}else{
+    Console.Write($"{n - r}");
 }
-else {
-    Console.Write($"{g}");
-}
+
+//Console.WriteLine("for");
+// for (int i = 0; i < n; i++)
+// {
+//     
+// }
+// Console.WriteLine();
+// Console.WriteLine("while");
+// int j = 0;
+// while (j < n) {
+//     Console.Write($"{array1[j]} ");
+//     j++;
+// }
+/*
+if(a > b)
+    b++;
+else if(a < b)
+    a++
+else
+    c++
+*/
+
+
+// int r = 0, g = 0;
+// for (int i = 0; i < n; i++)
+// {
+    // if (array1[i] == 1){
+    //     r++;
+    // }else{
+    //     g++;
+    // }
+// }
+
+// Console.WriteLine();
+// if (r < g){
+//     Console.Write($"{r}");
+// }
+// else {
+//     Console.Write($"{g}");
+// }
